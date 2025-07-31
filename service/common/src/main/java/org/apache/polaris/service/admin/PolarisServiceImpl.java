cadaba785fa90c477b4c77dfa3fa15727fb1c453
arun.suri <arun.suri@fivetran.com>
2025-08-01 04:07:26 +0530

    spotlessApply

--- service/common/src/main/java/org/apache/polaris/service/admin/PolarisServiceImpl.java
+++ service/common/src/main/java/org/apache/polaris/service/admin/PolarisServiceImpl.java
@@ -143,7 +143,7 @@ public class PolarisServiceImpl
   /** From PolarisCatalogsApiService */
   @Override
   public Response createCatalog(
-          CreateCatalogRequest request, RealmContext realmContext, SecurityContext securityContext) {
+      CreateCatalogRequest request, RealmContext realmContext, SecurityContext securityContext) {
     PolarisAdminService adminService = newAdminService(realmContext, securityContext);
     Catalog catalog = request.getCatalog();
     validateStorageConfig(catalog.getStorageConfigInfo());
@@ -258,7 +258,7 @@ public class PolarisServiceImpl
   /** From PolarisPrincipalsApiService */
   @Override
   public Response createPrincipal(
-          CreatePrincipalRequest request, RealmContext realmContext, SecurityContext securityContext) {
+      CreatePrincipalRequest request, RealmContext realmContext, SecurityContext securityContext) {
     PolarisAdminService adminService = newAdminService(realmContext, securityContext);
     PrincipalEntity principal =
         new PrincipalEntity.Builder()
@@ -278,10 +278,10 @@ public class PolarisServiceImpl
 
   @Override
   public Response resetCredentials(
-          String principalName,
-          ResetPrincipalRequest resetPrincipalRequest,
-          RealmContext realmContext,
-          SecurityContext securityContext) {
+      String principalName,
+      ResetPrincipalRequest resetPrincipalRequest,
+      RealmContext realmContext,
+      SecurityContext securityContext) {
     PolarisAdminService adminService = newAdminService(realmContext, securityContext);
     return Response.ok(adminService.resetCredentials(principalName, resetPrincipalRequest)).build();
   }
